d88b9f1da6b21d4a86c08b9a481b957ea2e8990a
angraween <angraween@gmail.com>
2021-05-20 21:04:46 -0300

    Arreglo errores y agrego .gitignore

--- pull.php
+++ pull.php
@@ -8,15 +8,14 @@
             foreach($persona as $valor){
                 echo "$valor <br>";
             }
-?>
 
-
-  $auto = [
+            $auto = [
                 "Marca" => "Ford",
                 "Modelo" => "Focus",
                 "Año" => "2017"
-          ];
+            ];
 
-          foreach($auto as $posicion => $valor){
-              echo "$posicion: $valor <br>";
-          }
+            foreach($auto as $posicion => $valor){
+                echo "$posicion: $valor <br>";
+            }
+?>
